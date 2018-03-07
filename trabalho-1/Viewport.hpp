977cc05c8b3ff265d310abca6469e599b2550705
Mauricio <mauriciombarbosa@hotmail.com>
2018-03-07 00:21:44 -0300

    comecando a implementar a viewports

---
+++ trabalho-1/Viewport.hpp
@@ -0,0 +1,17 @@
+#ifndef VIEWPORT_HPP
+#define VIEWPORT_HPP
+
+#include "Window.hpp"
+#include "Objects.hpp"
+#include "lista-encadeada.hpp"
+#include "elemento.hpp"
+
+class Viewport
+{
+  public:
+  protected:
+  private:
+
+};
+
+#endif
